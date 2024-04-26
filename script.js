2e23b9c555ddc0658e590ef61d60293196f47260
GloriaNketiah <164093629+GloriaNketiah@users.noreply.github.com>
2024-04-26 02:18:17 +0000

    savemadlib

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ var verb_2 = document.getElementById("verb_2").value;
 var sentence = "<p>" + "The law of inertia; also known as <i>" + noun + "</i> third law of motion is most <i>" + adjective  + "</i> in everyday life. It states that action and reaction are equal and opposite. This means that if an object is <i>" + verb + "</i> against a wall, it will <i>" + verb_2 + "</i> back with an equal force in the opposite direction." + "</p>";
   document.getElementById("output").innerHTML= sentence;
 }
+
+var sentence = document.getElementById("sentence").innerHTML;
+console.log("sentence: " + sentence);
+// function to save the madlib
+
+function saveMadlib() {
+  console.log("saveMadlib() called ");
+  var storyData = createMadlib();
+db.collection("madlibs").doc(storyData.storyName).set(storyData);
+  alert(storyData.storyName + " saved to database");
+}
